a1f4e22c23a057a5053b00cecc2ed3f6879674f4
Karthi <dhilipkarthi03@gmail.com>
2026-08-24 11:44:19 +0530

    sol change

--- Contracts/mint.sol
+++ Contracts/mint.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts@5.0.0/token/ERC721/extensions/ERC721URIStorage.sol";
@@ -6,12 +7,31 @@ import "@openzeppelin/contracts@5.0.0/access/Ownable.sol";
 contract MyNFT is ERC721URIStorage, Ownable {
     uint256 private _nextTokenId;
 
+    uint256 public constant MAX_SUPPLY = 1000;
+    uint256 public mintPrice = 0.001 ether;
+
     constructor() ERC721("MyNFT", "MNFT") Ownable(msg.sender) {}
 
-    function mint(address to, string memory uri) public returns (uint256) {
+    function mint(address to, string memory uri) public payable returns (uint256) {
+        require(_nextTokenId < MAX_SUPPLY, "Sold out");
+        require(msg.value >= mintPrice, "Insufficient payment");
+
         uint256 tokenId = _nextTokenId++;
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
         return tokenId;
+    }
+
+    function totalMinted() public view returns (uint256) {
+        return _nextTokenId;
+    }
+
+    function setMintPrice(uint256 newPrice) public onlyOwner {
+        mintPrice = newPrice;
+    }
+
+    function withdraw() public onlyOwner {
+        (bool success, ) = payable(owner()).call{value: address(this).balance}("");
+        require(success, "Withdraw failed");
     }
 }
